4cbbb3661331a0fa8634e14f6bf0dfa4bce9c59c
Dhruvik <dhruviksparikh@gmail.com>
2022-10-30 12:07:56 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/SpringMvc30102022Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc30102022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc30102022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Spring_MVC_30102022/SpringMvc30102022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Spring_MVC_30102022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc30102022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
